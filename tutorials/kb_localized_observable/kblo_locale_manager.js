b2f458a561e733b47d857f2baab5705bdc191aaf
Kevin Malakoff <xmann.intl@gmail.com>
2012-05-15 10:02:22 +0900

    Added label for when an observable is returned instead of an instance. Added tutorial for localized observable.

---
+++ tutorials/kb_localized_observable/kblo_locale_manager.js
@@ -0,0 +1,34 @@
+var LocaleManager = function() {
+  this.translations_by_locale = translations_by_locale;
+  this.current_locale = ko.observable(locale_identifier);
+
+  this.get = function(string_id) {
+    if (!this.translations_by_locale[this.current_locale()]) {
+      return '(no translation)';
+    }
+    if (!this.translations_by_locale[this.current_locale()].hasOwnProperty(string_id)) {
+      return '(no translation)';
+    }
+    return this.translations_by_locale[this.current_locale()][string_id];
+  };
+
+  this.getLocale = function() {
+    return this.current_locale();
+  };
+
+  this.setLocale = function(locale_identifier) {
+    this.current_locale(locale_identifier);
+    return this.trigger('change', this);
+  };
+};
+
+_.extend(LocaleManager.prototype, Backbone.Events);
+
+kb.locale_manager = new LocaleManager('en-GB', {
+  'en-GB': {
+    body: 'I ought to localize this sentence.'
+  },
+  'fr-FR': {
+    body: 'J\'ai besoin de localiser cette phrase.'
+  }
+});
