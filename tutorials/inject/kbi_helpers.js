322224a45557bb760cf3aba46d208c1a696f1a10
Kevin Malakoff <xmann.intl@gmail.com>
2012-09-26 22:48:00 -0700

    Update website for 0.16.7 release

--- tutorials/inject/kbi_helpers.js
+++ tutorials/inject/kbi_helpers.js
@@ -1,21 +1,12 @@
+// helper to toggle classes
 ko.bindingHandlers['classes'] = {
   update: function(element, value_accessor) {
-    classes = ko.utils.unwrapObservable(value_accessor());
+    var classes = ko.utils.unwrapObservable(value_accessor());
     for (var key in classes) {
-      var state = classes[key];
-      if (state) {
-        $(element).addClass(key);
-      } else {
-        $(element).removeClass(key);
-      }
-}}};
+      $(element)[ko.utils.unwrapObservable(classes[key]) ? 'addClass' : 'removeClass'](key);
+    }
+}};
 
-window.loadUrl = function(url) {
-  return Backbone.history.loadUrl(url);
-};
-
-window.loadUrlFn = function(url) {
-  return function() {
-    return Backbone.history.loadUrl(url);
-  };
-};
+// helpers to manage push state
+window.loadUrl = function(url) { return Backbone.history.loadUrl(url); };
+window.loadUrlFn = function(url) { return function() { return Backbone.history.loadUrl(url); }; };
