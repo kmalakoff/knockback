b1b7410ab06c92de20872cace5e19da1647f5575
Kevin Malakoff <kmalakoff@gmail.com>
2015-09-07 14:49:21 -0700

    Added test case for https://github.com/kmalakoff/knockback/issues/145

---
+++ test/issues/issue145/issue.js
@@ -0,0 +1,31 @@
+// MODIFIED from: https://stackoverflow.com/questions/15111361/backbone-localstorage-saves-but-does-not-fetch-collection-using-knockback
+
+var Player = Backbone.Model.extend({
+  urlRoot: '/players',
+  localStorage: new Backbone.LocalStorage('players'),
+  defaults: {name: 'Unnamed', team: 'Unassigned'}
+});
+var Players = Backbone.Collection.extend({
+  url: '/players',
+  model: Player,
+  localStorage: new Backbone.LocalStorage('players')
+});
+
+function GameViewModel() {
+  var self = this;
+
+  this.players = kb.collectionObservable(new Players());
+
+  console.log('BEFORE fetch: ', self.players.collection().models);
+  this.players.collection().fetch({success: function() {
+    console.log('AFTER fetch: ', self.players.collection().models);
+  }});
+};
+
+// save one if none exist yet
+var players = new Players();
+if (!players.localStorage.findAll().length) {
+  new Player().save({name: _.uniqueId('Player ')});
+}
+
+ko.applyBindings(new GameViewModel());
