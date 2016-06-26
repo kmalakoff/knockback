a2ff99e1516a3fea36e264aad42c6def6a57f423
Kevin Malakoff <kmalakoff@gmail.com>
2016-06-26 14:16:19 -0700

    Simplify 157

--- test/issues/issue157/issue.js
+++ test/issues/issue157/issue.js
@@ -2,25 +2,15 @@ var SortItemModel = Backbone.Model.extend({});
 
 var ViewModel = kb.ViewModel.extend({
   i: 1,
-  constructor: function (model, options)
-  {
-    options          = options || {};
-    options.excludes = ['lines'];
-    this.lines = new kb.CollectionObservable(model.get('lines'), kb.ViewModel);
-    kb.ViewModel.apply(this, [model, options]);
-    return this;
-  },
 
   addItem: function ()
   {
     var obj = {name: "test" + (this.i++)};
     var line = new SortItemModel(obj);
-    this.lines.push(new kb.ViewModel(line));
-    console.log("CollectionObservable length:", this.lines.length);
-    console.log("Collection length:", this.lines.collection().length);
+    this.model().get('lines').push(line);
+    console.log("CollectionObservable length:", this.lines().length);
+    console.log("Collection length:", this.model().get('lines').length);
   }
 });
-
-var view_model = new ViewModel(new Backbone.Model({lines: new Backbone.Collection()}));
 
 ko.applyBindings(view_model, $('#some-div')[0]);
