4989f2b28f264867fb0574d903315f8011dcb5f0
Kevin Malakoff <kmalakoff@gmail.com>
2017-03-13 21:46:38 -0700

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,24 @@
 module.exports = {
-  "extends": "airbnb-base",
-  "plugins": [
-    "import"
+  'extends': 'airbnb-base',
+  'plugins': [
+    'import'
   ],
-  "env": {
-    "es6": true,
-    "node": true,
-    "mocha": true
+  'env': {
+    'es6': true,
+    'node': true,
+    'mocha': true
+  },
+  'rules': {
+    // http://eslint.org/docs/rules/max-len
+    'max-len': ['error', 200, 2],
+
+    // http://eslint.org/docs/rules/camelcase
+    'camelcase': 'off',
+
+    // http://eslint.org/docs/rules/no-underscore-dangle
+    'no-underscore-dangle': 'off',
+
+    // http://eslint.org/docs/rules/no-underscore-dangle
+    'import/no-unresolved': 'off',
   }
 };
